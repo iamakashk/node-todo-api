1baf64feaaa6db1cfc4a3b4e914ccee503142b2b
AKASH KAMBLE <kambleakashg@gmail.com>
2018-07-06 22:08:42 +0530

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var app = express();
 
+//heroku or localhost
+//check package.json start and engines
+//https://elements.heroku.com/addons/mongolab
+var port = process.env.PORT || 3000;
+
 const {ObjectID} = require('mongodb');
 app.use(bodyParser.json());
 
@@ -59,8 +64,8 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 })
 
 // var Todo = mongoose.model('Todo', {
